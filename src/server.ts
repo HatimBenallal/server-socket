56f3e8b49b20a41ba536092584ce8d4e4a46a50e
hatimb99 <hatim.ben99@gmail.com>
2022-11-11 17:22:42 +0100

    server

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,19 @@ import mongoose, { ConnectOptions } from "mongoose";
 import cors from "cors";
 import User from "./api/User";
 import Booking from "./api/Booking";
-
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 
 const app = express();
 const port = process.env.PORT || 5432;
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+	origin:true,
+	credentials:true,
+	methods:["GET","POST"]
+  }
+});
 
 // Middlewares
 app.use(bodyParser.urlencoded({ extended : true }));
@@ -23,8 +32,19 @@ app.get('/', ( req: express.Request, res: express.Response ) => {
 	res.send('Hello World!')
 })
 
+
+io.on("connection", (socket: Socket) => {
+	console.log("new user connected");
+	socket.on("sendMessage",(messageInfo) => {
+		console.log("sending message");
+		socket.broadcast.emit("receiveMessage", messageInfo); 
+	});
+});
+httpServer.listen(3000);
+
+
 // Database
-mongoose.connect('mongodb://localhost/users', { useNewUrlParser : true } as ConnectOptions)
+mongoose.connect('mongodb://0.0.0.0/users', { useNewUrlParser : true } as ConnectOptions)
 	.then(() => {
 		// tslint:disable-next-line:no-console
         app.listen(port, () => console.log("Server corriendo en el puerto " + port));
